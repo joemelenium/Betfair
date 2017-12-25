1f708b9c63a2df2b17beb58ac255ac5150861695
joemelenium <joseph.gamble.24a@gmail.com>
2017-12-25 21:29:49 +0000

    25/12/17

--- GetWallet.py
+++ GetWallet.py
@@ -1,4 +1,18 @@
-colours  = ["red", "Yello", "Blue"]
-print: "hello"
+import urllib
+appKey = "tfs8j951cmP5ViW0"
+sessionToken =  "1lDrIyhwahzJ97leBU8L0De5xgmPo2QHOZW16XsJDnE="
 
+import requests
+import json
 
+endpoint = "https://api.betfair.com/exchange/betting/rest/v1.0/"
+
+header = {'X-Application': appKey, 'X-Authentication': sessionToken, 'content-type': 'application/json'}
+
+json_req = '{"filter":{ }}'
+
+url = endpoint + "listEventTypes/"
+
+response = requests.post(url, data=json_req, headers=header)
+
+print(json.dumps(json.loads(response.text), indent=3))
